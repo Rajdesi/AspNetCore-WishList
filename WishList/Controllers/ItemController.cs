50ba8af3f55da47df8284183e9591ae7ac5dbc75
Raj Desi <desiraj7679@gmail.com>
2022-11-12 01:23:59 +0530

    Code check in

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public ItemController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var model = _context.Item.ToList();
+            return View("Index", model);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+        [HttpPost]
+        public IActionResult Create(Models.Item item)
+        {
+            _context.Item.Add(item);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int Id)
+        {
+            var item = _context.Item.FirstOrDefault(e => e.Id == Id);
+            _context.Item.Remove(item);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
--- WishList/Program.cs
+++ WishList/Program.cs
@@ -15,6 +15,7 @@ namespace WishList
         public static void Main(string[] args)
         {
             BuildWebHost(args).Run();
+
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
